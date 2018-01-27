9155982edea2c5d229aa9ff14dd1875804b2b2b0
Ischa <Soeteweyischa@gmail.com>
2018-01-27 16:36:06 +0100

    CameraScript

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private Camera _camera;
+    public Transform PlayerPos;
+    
+
+    public Transform ZoomOutLocation;
+
+    public float CameraOffsetY = 2.3f;
+    public float CameraOffSetZ = -2.3f;
+    private Vector3 CameraOffSet;
+
+    // Use this for initialization
+    void Start () {
+        _camera = Camera.main;
+        CameraOffSet = new Vector3(0, CameraOffsetY, CameraOffSetZ);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        FollowPlayer();
+        ZoomOut();
+    }
+
+    private void FollowPlayer()
+    {
+        _camera.transform.position = PlayerPos.position + CameraOffSet;
+        _camera.transform.LookAt(PlayerPos);
+    }
+
+    private void ZoomOut()
+    {
+        if (Input.GetButton("CameraZoom"))
+        {
+            _camera.transform.position = ZoomOutLocation.position;
+            _camera.transform.eulerAngles = ZoomOutLocation.eulerAngles;
+            Debug.Log("Button down");
+        }
+    }
 }
