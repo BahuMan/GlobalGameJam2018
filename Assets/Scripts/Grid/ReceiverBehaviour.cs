cfeca43190be006d71e34b459f1727c7686efb3b
Bart Huylebroeck <bart.huylebroeck@pandora.be>
2018-01-27 21:30:17 +0100

    oops

---
+++ Assets/Scripts/Grid/ReceiverBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ReceiverBehaviour: MonoBehaviour
+{
+    [SerializeField]
+    private float _timeBetweenScore = 1;
+    private float _nextScoreTime = 0;
+    
+    private void Start()
+    {
+        CellBehaviour cell = GetComponent<CellBehaviour>();
+        cell.OnLightSwitched += Cell_LightSwitched;
+        this.enabled = false;
+    }
+
+    private void Cell_LightSwitched(bool light)
+    {
+        this.enabled = true;
+        Debug.Log("Goal is lit up");
+    }
+
+}
+
