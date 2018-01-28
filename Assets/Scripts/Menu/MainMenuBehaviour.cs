4ff8908cd4a41630e527d32b358d030d3b2e4c90
Bart Huylebroeck <bart.huylebroeck@pandora.be>
2018-01-28 11:21:44 +0100

    main menu quit and play buttons OK OnSignalPropagated event

---
+++ Assets/Scripts/Menu/MainMenuBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuBehaviour : MonoBehaviour {
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
