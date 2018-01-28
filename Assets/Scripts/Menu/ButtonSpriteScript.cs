d4254a7927c0019c8866c8ed4f1f560044ebfa8c
Ischa <Soeteweyischa@gmail.com>
2018-01-28 02:41:30 +0100

    Base Menu

---
+++ Assets/Scripts/Menu/ButtonSpriteScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSpriteScript : MonoBehaviour {
+
+    public Sprite HoverSprite;
+    public Sprite ClickSprite;
+    public Sprite BaseSprite;
+
+    private Sprite _view;
+
+    private void Start()
+    {
+        _view = GetComponent<Image>().sprite;
+    }
+
+    public void OnHover()
+    {
+        _view = HoverSprite;
+    }
+
+    public void OnClick()
+    {
+        _view = ClickSprite;
+    }
+
+    public void OnExit()
+    {
+        _view = BaseSprite;
+    }
+}
